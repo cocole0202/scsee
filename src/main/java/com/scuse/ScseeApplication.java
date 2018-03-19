c5821be06db5f506d7859fda1c75222c373d92f4
JahoJiang <JahoJiang@163.com>
2018-03-19 09:25:11 +0800

    Refactor

--- src/main/java/com/scuse/ScseeApplication.java
+++ src/main/java/com/scuse/ScseeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScseeApplication {
 
 	public static void main(String[] args) {
-        SpringApplication.run(ScseeApplication.class, args);
+
+		SpringApplication.run(ScseeApplication.class, args);
 	}
 }
